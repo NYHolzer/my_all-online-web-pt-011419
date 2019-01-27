e27153cf86bff18ac6a0a4f5a4ece609fe0cee75
Nissan Holzer <nissan.holzer@gmail.com>
2019-01-27 01:06:28 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,14 +1,14 @@
 require 'pry'
 
-def my_all?(collection)
-  i = 0 
-  block_return_values = []
-  while i < collection.length
-    block_return_values << yield(collection[i])
-    i += 1 
+def my_all?(collection)    #defining method with argument
+  i = 0                    #setting a counter
+  block_return_values = []   #setting a blank array to store yield return values 
+  while i < collection.length    #set while statement to loop through the collection
+    block_return_values << yield(collection[i])  #adding yield return values to array 
+    i += 1                   #counter
   end
   
-  if block_return_values.include?(false)
+  if block_return_values.include?(false)     #setting return values of method
     false
   else
     true 
